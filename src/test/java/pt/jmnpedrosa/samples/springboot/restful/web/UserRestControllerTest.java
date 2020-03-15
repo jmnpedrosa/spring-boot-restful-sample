839d79d5c14855e0c2ffb6371d65125d8d0809da
José Pedrosa <jmnpedrosa@gmail.com>
2020-03-15 14:03:01 +0000

    Migrate unit tests to JUnit 5.
    - Upgraded test classes to JUnit 5.
    - Excluded JUnit 4 vintage engine dependency.

--- src/test/java/pt/jmnpedrosa/samples/springboot/restful/web/UserRestControllerTest.java
+++ src/test/java/pt/jmnpedrosa/samples/springboot/restful/web/UserRestControllerTest.java
@@ -12,15 +12,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -31,7 +29,6 @@ import pt.jmnpedrosa.samples.springboot.restful.error.UserNotFoundException;
 import pt.jmnpedrosa.samples.springboot.restful.model.User;
 import pt.jmnpedrosa.samples.springboot.restful.service.DefaultUserService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(UserRestController.class)
 public class UserRestControllerTest {
 
@@ -43,7 +40,7 @@ public class UserRestControllerTest {
   @Autowired
   private WebApplicationContext webApplicationContext;
 
-  @Before
+  @BeforeEach
   public void setUp() {
     Mockito.reset(defaultUserService);
     webMockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
